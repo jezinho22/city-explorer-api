d6c1047d09016d5794226de9fc13c8376a766b3c
Jeremy Johns <jez.johns@gmail.com>
2023-05-12 15:21:56 +0000

    undid modularising attempt

--- server.js
+++ server.js
@@ -7,50 +7,32 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 
-// const weather = require("./data/weather.json");
-// filter function
-// function filterData(lon, lat, searchQuery) {
-// 	let result = [];
-// 	if (searchQuery) {
-// 		// this led to getting the answer as an array, but I was trying yo
-// 		// treat it as an object - took a while to work out
-// 		result = weather.filter((location) => location.city_name == searchQuery);
-// 	} else if (lon && lat) {
-// 		result = weather.filter(
-// 			(location) => location.lon == lon && location.lat == lat
-// 		);
-// 	}
-// 	return result;
-// }
-// HTTP: http://api.weatherbit.io/v2.0/forecast/daily
-// days= up to 16 & lat & lon
+// endpoint for root
+app.get("/", (request, response) => response.json("You reached the root"));
 
-async function getWeather(lon, lat) {
-	const API = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}&days=7`;
-	let res = await axios.get(API);
-	console.log(res.data.city_name);
-	return res.data;
-	// response.json(res);
-}
-// functions to put together weather forecast data to send user
-function dayForecast(day) {
-	return {
-		date: day.datetime,
-		description: `Low of ${day.min_temp} degrees rising to high of ${
-			day.max_temp
-		} with ${day.weather.description.toLowerCase()}`,
-	};
-}
-
-// look at Tim's error handlers
-// object.data.prop
-function forecast(weatherDays) {
-	return weatherDays.map((day) => {
+// endpoint for weather
+app.get("/weather", async (request, response) => {
+	try {
+		const { lat, lon } = request.query;
+		const API = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}&days=7`;
+		const results = await axios.get(API); //returns result.data and we want .data
+		// set up data to return to client
+		const forecasts = makeForecasts(results.data.data);
+		console.log(results.data.city_name);
+		response.json(forecasts);
+	} catch (error) {
+		console.log(error);
+		const forecasts = {};
+		response.json(forecasts);
+	}
+});
+function makeForecasts(days) {
+	return days.map((day) => {
 		return {
 			date: day.valid_date,
 			description: `Low of ${day.min_temp} degrees and high of ${
 				day.max_temp
-			} degrees with ${day.weather.description.toLowerCase()}`,
+			} degrees with ${day.weather.description.toLowerCase()}.`,
 		};
 		console.log(day.valid_date);
 	});
@@ -61,23 +43,5 @@ function forecast(weatherDays) {
 	// forecasts.push(fc)
 	//})
 }
-
-// endpoint for root
-app.get("/", (request, response) => response.json("You reached the root"));
-
-// endpoint for weather
-app.get("/weather", async (request, response) => {
-	// const { lat, lon, searchQuery } = request.query;
-	try {
-		// get from the weather api
-		let lat = request.query.lat;
-		let lon = request.query.lon;
-		let results = await getWeather(lon, lat);
-		let weatherForecast = forecast(results.data); // results.data is an array
-		response.json(weatherForecast);
-	} catch (error) {
-		response.json("Error in coordinates");
-	}
-});
 
 app.listen(PORT, () => console.log("App running on port " + PORT));
